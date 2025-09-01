e23720c3dc421e9b2efe2a5d55dcf90bcc651522
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2025-09-01 20:50:56 -0300

    chore: Adicionar modelo do projeto ToDoList

---
+++ ToDoListPOO/src/main/java/todolist/aplicativo/model/Tarefa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package todolist.aplicativo.model;
+
+/**
+ *
+ * @author ruben
+ */
+public class Tarefa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
