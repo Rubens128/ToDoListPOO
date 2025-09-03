36e5f572bdb4dddd66086e3d705fb1d0785b1341
pedroedu06 <pedro.nascimento182501@gmail.com>
2025-09-03 20:59:43 -0300

    Adicionando os setters

--- ToDoListPOO/src/main/java/todolist/aplicativo/model/Tarefa.java
+++ ToDoListPOO/src/main/java/todolist/aplicativo/model/Tarefa.java
@@ -14,10 +14,13 @@ import java.time.LocalDate;
 public class Tarefa {
 
 private long id;
+  
+    private long id;
     private String titulo;
     private String descricao;
     private boolean completa;
     private LocalDate dataAgora = LocalDate.now();
+    private LocalDate dataConclusao;
     
     public Tarefa(long id, String titulo, String descricao, boolean completa, LocalDate dataAgora) {
         this.Id = id;
@@ -26,6 +29,15 @@ private long id;
         this.Completa = false;
         this.DataAgora = dataAgora;
     }
+    
+    public Tarefa(String titulo, String descricao) {
+        this.id = 0;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.completa = false;
+        this.dataCriacao = LocalDate.now();
+    }
+    /* so para teste */
     
     public String getTitulo() {
     	return titulo;
@@ -42,12 +54,41 @@ private long id;
     public LocalDate getDataAgora() {
     	return dataAgora;
     }
-
+    
     public String getListarTarefa(String titulo, String descricao) {
         this(titulo, descricao);
     }
 
     public void completar(boolean Completa){
         this.Completa = true;
+    }
+    
+    public void setID(long id) {
+    	this.id = id;
+    }
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public void setDataAgora(LocalData dataAgora) {
+    	this.dataAgora = dataAgora;
+    }
+    
+    public void setCompletar(boolean completa) {
+    	this.completa = completa;
+    	if (completa) {
+    		this.dataConclusao = LocalDate.now();
+    	} else {
+    		this.dataConclusao = null;
+    	}
+    }
+    
+    public void completar() {
+    	setCompletar = true;
     }
 }
