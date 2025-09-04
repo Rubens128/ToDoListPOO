85217e3be33d90a39567b5d551677c3a66cf6b7e
predrim <pedrofsp2@gmail.com>
2025-09-04 19:20:27 -0300

    Adicionados os comandos e a interface cli para manipulação da todo list

--- ToDoListPOO/src/main/java/todolist/aplicativo/view/ToDoList.java
+++ ToDoListPOO/src/main/java/todolist/aplicativo/view/ToDoList.java
@@ -4,10 +4,142 @@
  */
 package todolist.aplicativo.view;
 
+import todolist.aplicativo.model.Tarefa;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 /**
  *
  * @author ruben
  */
 public class ToDoList {
+    public static void toDoList() {
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Tarefa> tarefas = new ArrayList<>();
+    int contadorID = 1;
+    int escolha = 0;
     
+    
+    while(escolha != 6) {
+        System.out.println("### ToDo List ###\n" +
+                           "1. Cadastrar tarefa\n" +
+                           "2. Visualizar tarefas\n" +
+                           "3. Editar tarefa\n" +
+                           "4. Excluir tarefa\n" +
+                           "5. Marcar tarefa como concluida\n" +
+                           "6. Sair\n" +
+                           "Digite o número correspondente à sua escolha: ");
+        escolha = scan.nextInt();
+        scan.nextLine();
+    
+        switch (escolha) {
+            case 1:
+                System.out.println("\nInsira o título da tarefa: ");
+                String titulo = scan.nextLine();
+                System.out.println("Insira a descrição da tarefa: ");
+                String descricao = scan.nextLine();
+                
+                Tarefa novaTarefa = new Tarefa(titulo, descricao);
+                novaTarefa.setID(contadorID++);
+                tarefas.add(novaTarefa);
+                System.out.println("Tarefa adicionada com sucesso!");
+                
+                break;
+            
+            case 2:
+                System.out.println("\nTarefas Cadastradas:");
+                if (tarefas.isEmpty()) {
+                    System.out.println("Nenhuma tarefa encontrada.");
+                } else {
+                    for (Tarefa tarefa : tarefas) {
+                        System.out.println(tarefa);
+                    }
+                }
+                break;
+                
+            case 3:
+                System.out.println("\nDigite o ID da tarefa que deseja editar: ");
+                long escolhaID = scan.nextInt();
+                scan.nextLine();
+                
+                if (tarefas.isEmpty()) {
+                    System.out.println("Nenhuma tarefa encontrada.");
+                } else {
+                    for (Tarefa tarefa : tarefas) {
+                        if (tarefa.getID() == escolhaID) {
+                            System.out.println("Insira um novo título para a tarefa: ");
+                            String novoTitulo = scan.nextLine();
+                        
+                            System.out.println("Insira uma nova descrição para a tarefa: ");
+                            String novaDescricao = scan.nextLine();
+                        
+                            tarefa.setTitulo(novoTitulo);
+                            tarefa.setDescricao(novaDescricao);
+                            
+                            System.out.println("Tarefa editada com sucesso!");
+                        }
+                    }
+                }  
+                
+                break;
+                
+            case 4:
+                if (tarefas.isEmpty()) {
+                    System.out.println("Nenhuma tarefa encontrada.");
+                    break;
+                }
+                
+                System.out.println("\nDigite o ID da tarefa que deseja excluir: ");
+                long excluirID = scan.nextInt();
+                scan.nextLine();
+                
+                for (Tarefa tarefa : tarefas) {
+                        if (tarefa.getID() == excluirID) {
+                        
+                            tarefas.remove(tarefa);
+                            
+                            System.out.println("Tarefa excluida com sucesso!");
+                        }
+                        else {
+                            System.out.println("Tarefa não encontrada.");
+                        }
+                    }
+                break;
+            
+            case 5:
+                if (tarefas.isEmpty()) {
+                    System.out.println("Nenhuma tarefa encontrada");
+                    break;
+                }
+                
+                System.out.println("\nDigite o ID da tarefa que deseja concluir: ");
+                long concluirID = scan.nextInt();
+                scan.nextLine();
+                
+                for (Tarefa tarefa : tarefas) {
+                        if (tarefa.getID() == concluirID) {
+                        
+                            tarefa.completar();
+                            
+                            System.out.println("Tarefa concluida com sucesso!");
+                        }
+                        else {
+                            System.out.println("Tarefa não encontrada.");
+                        }
+                    }
+                
+                break;
+                
+            case 6:
+                System.out.println("Saindo...");
+                
+                break;
+                
+            default:
+                System.out.println("Opção inválida.");
+        }
+    
+    }
+    
+    }
 }
