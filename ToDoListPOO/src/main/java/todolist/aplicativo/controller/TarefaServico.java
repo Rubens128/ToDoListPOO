ebe347c8142c7df8d0431991a1fa0f5f2c346aef
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2025-09-04 22:21:16 -0300

    feat: Criação dos metodos de criar e listar na classe TarefaServico

--- ToDoListPOO/src/main/java/todolist/aplicativo/controller/TarefaServico.java
+++ ToDoListPOO/src/main/java/todolist/aplicativo/controller/TarefaServico.java
@@ -4,10 +4,44 @@
  */
 package todolist.aplicativo.controller;
 
+import java.util.ArrayList;
+import todolist.aplicativo.model.Tarefa;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 /**
  *
  * @author ruben
  */
 public class TarefaServico {
+    public static void criar(ArrayList<Tarefa> lista, String titulo, String descricao, int id){
+        
+        Tarefa novaTarefa = new Tarefa(titulo, descricao);
+        
+        novaTarefa.setID(id);
+        
+        lista.add(novaTarefa);
+        
+    }
     
+    public static ArrayList<String> listar(ArrayList<Tarefa> lista){
+        
+        ArrayList<String> dadosTarefas = new ArrayList<>();
+        
+        for(Tarefa item: lista){
+            
+            DateTimeFormatter dataFormato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            
+            String dadosDaTarefa = "\nID: " + item.getID() + "\n" + "Titulo: " + item.getTitulo() + "\n";
+            dadosDaTarefa += "Descricao: "+ item.getDescricao() + "\n";
+            dadosDaTarefa += "Completado: "+(item.getComplete()? "Sim" : "Nao") + "\n";
+            dadosDaTarefa += "Data de Inicio: " + (item.getDataAgora().format(dataFormato)) + "\n";
+            if(item.getDataConclusao() != null) dadosDaTarefa += "Data de Conclusao: " + (item.getDataConclusao().format(dataFormato)) + "\n\n";
+            else dadosDaTarefa += "Data de Conclusao: Nao concluido \n\n";
+                
+            dadosTarefas.add(dadosDaTarefa);
+            
+        }
+        
+        return dadosTarefas;
+    }
 }
